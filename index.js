9d2d4f833e7ba01c032d4a9e9c90dabd3ff8a343
Valerio Donati <vdonati@newstore.com>
2015-07-30 19:48:36 +0200

    date format + custom headers + random amount functions

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
+require("date-format-lite");
+
 var config = require('config'),
     CronJob = require('cron').CronJob,
     faker = require('faker')
     fs = require('fs'),
     request = require('request');
 
+function getRandomAmount(min, max) {
+  const TAX_RATE = 19;
+
+  var amount = {};
+
+  amount.net = Math.round((Math.random() * (max - min) + min) * 100) / 100;
+  amount.gross = Math.round(amount.net * (TAX_RATE));
+  amount.tax_amount = Math.round(amount.gross - amount.net);
+  amount.tax_rate = TAX_RATE;
+
+  return amount;
+}
+
 function generateOrder() {
-  var requestBody = {
-    billing_address: {
-      country: faker.address.country(),
-      street: faker.address.streetAddress(),
-      zip: faker.address.zipCode()
-    },
-    external_id: faker.random.number(),
-    placed: faker.date.past,
-    shipping_address: {
-      country: faker.address.country(),
-      street: faker.address.streetAddress(),
-      zip: faker.address.zipCode()
-    },
-    uuid: faker.random.uuid()
+  var orderAmount = getRandomAmount(10, 100);
+  var consumerName = {
+    first_name: faker.name.firstName,
+    last_name: faker.name.lastName
   };
+  var orderDate = new Date(faker.date.past()).format("iso");
+  var requestBody = {
+    external_id: faker.random.uuid(),
+    sales_order_items: [],
+    net: orderAmount.net,
+    gross: orderAmount.gross,
+    tax_amount: orderAmount.tax_amount,
+    tax_rate: orderAmount.tax_rate,
+    currency: "EUR",
+    shipping_address: {
+      first_name: consumerName.first_name,
+      last_name: consumerName.last_name,
+      country: faker.address.country(),
+      street: faker.address.streetAddress(),
+      zip: faker.address.zipCode()
+    },
+    billing_address: {
+      first_name: consumerName.first_name,
+      last_name: consumerName.last_name,
+      country: faker.address.country(),
+      street: faker.address.streetAddress(),
+      zip: faker.address.zipCode()
+    },
+    placed: orderDate
+  }
 
   return requestBody;
 }
@@ -33,15 +63,23 @@ function logUUID(uuid) {
 }
 
 function postRequest(requestBody) {
-  request.post(
-    config.get('es_endpoint'),
-    {json: requestBody},
-    function (error, response) {
-      if (response.statusCode === 201) {
-        logUUID(requestBody.uuid)
-      }
+  request({
+    method: 'POST',
+    url: config.get('es_endpoint'),
+    headers: {
+      'Authorization': config.get('auth_token'),
+      'Content-Type': 'application/json'
+    },
+    json: requestBody
+  },
+  function (error, response) {
+    if (response.statusCode === 201) {
+      console.log(response);
+      logUUID(requestBody.uuid)
+    } else {
+      console.log(response);
     }
-  );
+  });
 }
 
 new CronJob('*/'+config.get('wait_between_request_blocks')+' * * * * *', function() {
